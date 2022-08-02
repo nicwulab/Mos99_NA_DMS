4fafc290d1c9a0601d374c94edaef9729e098b44
Nicholas Wu <wchnicholas@wirelessprv-10-194-238-30.near.illinois.edu>
2022-08-02 14:54:44 -0500

    update analysis

--- script/plot_pos_type_analysis.R
+++ script/plot_pos_type_analysis.R
@@ -8,6 +8,7 @@ library(reshape)
 library(stringr)
 library(plyr)
 library(dplyr)
+library(qualpalr)
 library(gridExtra)
 library(ggbeeswarm)
 library(sinaplot)
@@ -16,9 +17,10 @@ require(cowplot)
 
 plot_RSA_vs_fit <- function(fit_table, graphname){
   textsize <- 7
-  palette  <- c(brewer.pal(9,"Set1"))
-  p <- ggplot(fit_table,aes(x=RSA_tetramer, y=fit, color=type)) +
-         geom_point(size=0.2) +
+  #palette  <- c(brewer.pal(9,"Set1"))
+  palette <- qualpal(n = 5, list(h = c(0, 360), s = c(0.4, 0.6), l = c(0.5, 0.85)))$hex
+  p <- ggplot(fit_table,aes(x=RSA_tetramer*100, y=fit, color=type)) +
+         geom_point(size=0.5, pch=16, alpha=0.8) +
          theme_cowplot(12) +
          theme(plot.title=element_blank(),
                plot.background = element_rect(fill = "white"),
@@ -32,54 +34,63 @@ plot_RSA_vs_fit <- function(fit_table, graphname){
                legend.position='right') +
          guides(colour = guide_legend(override.aes = list(size=0.5))) +
          scale_color_manual(values=palette,drop=FALSE) +
-         labs(x=expression(bold("RSA ("*Å^"2"*")")),y=expression(bold('fitness')))
-  ggsave(graphname, p, height=2, width=3.5, dpi=300)
+         labs(x=expression(bold("RSA (%)")),y=expression(bold('mutational tolerance')))
+  ggsave(graphname, p, height=2, width=3, dpi=600)
   }
 
-plot_position_type <- function(data_table, graphname, ylab){
+plot_position_type <- function(data_table, graphname, ylab,violin){
   summary <- ddply(data_table,c("type"), summarise, mean = mean(parameter), sd = sd(parameter))
   textsize <- 7
-  palette  <- c(brewer.pal(9,"Set1"))
-  p <-  ggplot() +
-          geom_sina(data=data_table,aes(x=type,y=parameter,color=type), size=0.1,
-                    method="counts", bin_limit=0.4, scale="width", maxwidth=0.5) +
-          #geom_violin(data=data_table,aes(x=type,y=parameter,fill=type), size=0.1, scale='area') +
-          #geom_beeswarm(data=data_table,aes(x=type,y=parameter,color=type), size=0.1) +
-          geom_errorbar(data=summary, mapping=aes(x=type, ymin=mean-sd, ymax=mean+sd),
-                        size=0.3, color="black", width=0.2, position=position_dodge(1)) +
-          geom_point(data=summary, mapping = aes(x=type, y=mean),
-                     size=4, color="black", shape="-", position=position_dodge(1)) +
+  if (violin=='yes'){
+    p <-  ggplot() +
+            geom_violin(data=data_table,aes(x=type,y=parameter),size=0.3, scale='area', width=1)
+    }
+  else{
+    p <-  ggplot()
+    }
+  p <- p +
+          geom_boxplot(data=data_table,aes(x=type,y=parameter),width=0.2, outlier.shape=NA, size=0.3) +
+          geom_sina(data=data_table,aes(x=type,y=parameter),
+                    pch=16, size=0.1,method="counts", bin_limit=0.4, scale="width", maxwidth=0.5, color='gray40', alpha=0.5) +
           theme_cowplot(12) +
           theme(plot.background = element_rect(fill = "white"),
                 axis.text=element_text(size=textsize,face="bold",colour = 'black'),
                 axis.text.x=element_text(angle=90,hjust=1,vjust=0.5,colour = 'black'),
                 axis.text.y=element_text(hjust=0.5,vjust=0.5,colour = 'black'),
                 axis.title=element_text(size=textsize,face="bold"),
-                #axis.line = element_line(colour = 'black', size = 0),
-                #panel.border = element_rect(colour = "black", fill=NA, size=1), 
                 legend.title=element_blank(),
                 legend.key.height = unit(0.15, 'in'),
                 legend.key.width = unit(0.05, 'in'),
                 legend.text=element_text(size=textsize,face="bold"),
                 legend.position='right') +
           guides(colour = guide_legend(override.aes = list(size=0.5))) +
-          scale_color_manual(values=palette,drop=FALSE) +
           xlab("") +
           ylab(ylab)
-  ggsave(graphname, p, width=3.5, height=3.5, dpi=300)
+  ggsave(graphname, p, width=2, height=2.2, dpi=600)
   }
 
 set.seed(26)
-position_types <- c('active site','antigenic site',
-                    'buried','exposed','interface')
+position_types <- c('buried','interface','active site','antigenic','exposed')
 fit_table <- read_tsv('result/position_type_vs_fit.tsv') %>%
                mutate(type=factor(type, level=position_types))
-plot_RSA_vs_fit(fit_table, 'graph/RSA_vs_fit.png')
+plot_RSA_vs_fit(fit_table, 'graph/fit_vs_RSA.png')
+print (paste('Pearson correlation between RSA and fitness: ', cor(fit_table$RSA_tetramer, fit_table$fit, method="pearson"), sep=''))
+print (paste('Spearman correlation between RSA and fitness: ', cor(fit_table$RSA_tetramer, fit_table$fit, method="spearman"), sep=''))
+
 RSA_table <- fit_table %>%
+               mutate(RSA_tetramer=RSA_tetramer*100) %>%
                select(type, RSA_tetramer) %>%
                rename(parameter=RSA_tetramer)
 FIT_table <- fit_table %>%
                select(type, fit) %>%
                rename(parameter=fit)
-plot_position_type(RSA_table, 'graph/position_type_vs_RSA.png', expression(bold("RSA ("*Å^"2"*")")))
-plot_position_type(FIT_table, 'graph/position_type_vs_fit.png', expression(bold("fitness")))
+plot_position_type(RSA_table, 'graph/position_type_vs_RSA.png', expression(bold("RSA (%)")),'no')
+plot_position_type(FIT_table, 'graph/position_type_vs_fit.png', expression(bold("mutational tolerance")),'yes')
+
+for (pos_type1 in position_types){
+  for (pos_type2 in position_types){
+    p_value <- t.test(filter(fit_table,type==pos_type1)$fit, filter(fit_table,type==pos_type2)$fit)$p.value
+    #p_value <- t.test(filter(fit_table,type==pos_type1)$RSA_tetramer, filter(fit_table,type==pos_type2)$RSA_tetramer)$p.value
+    print (paste(pos_type1, 'vs', pos_type2, ':', p_value))
+    }
+  }
