97609128fef3d3e654faf34cccd3b770b79a9dfe
Nicholas Wu <wchnicholas@Nicholass-MacBook-Pro-2.local>
2022-11-11 23:17:38 -0600

    update ddG and MSA transformer analysis

--- script/plot_fit_vs_predict.R
+++ script/plot_fit_vs_predict.R
@@ -39,9 +39,15 @@ plot_MSA_transformer <- function(t, graphname){
   ggsave(graphname, p, height=1.5, width=1.5, dpi=600)
   }
 
-plot_ddG <- function(t, graphname){
+plot_ddG <- function(t, graphname, form){
   textsize <- 7
-  p <- ggplot(t,aes(x=ddG_foldx, y=Fitness)) +
+  if (form=='tetramer'){
+    t <- rename(t, ddG=ddG_tetramer)
+    }
+  if (form=='monomer'){
+    t <- rename(t, ddG=ddG_monomer)
+    }
+  p <- ggplot(t,aes(x=ddG, y=Fitness)) +
          geom_point(size=0.5, color='black',alpha=0.2, pch=16) +
          #geom_smooth(method="loess", se=FALSE, fullrange=FALSE, level=0.95, color='blue' ,size=0.3) +
          #stat_smooth(method="loess", se=FALSE, span=0.1, color='blue' ,size=0.3) +
@@ -62,8 +68,13 @@ plot_ddG <- function(t, graphname){
   ggsave(graphname, p, height=1.5, width=1.5, dpi=600)
   }
 
-ceiling <- function(x){
+ceiling_1 <- function(x){
   if (x>30){return (30)}
+  else {return (x)}
+  }
+
+ceiling_2 <- function(x){
+  if (x>8){return (8)}
   else {return (x)}
   }
 
@@ -71,15 +82,22 @@ fit  <- read_csv('result/Mos99_fit.csv') %>%
           rename(mut=Mutation)
 pred <- read_csv('data/foldx_msa_transformer.csv') %>%
           inner_join(fit, by='mut') %>%
-          mutate(pos=as.numeric(str_sub(mut, 2, -2))) %>%
-          mutate(ddG_foldx=mapply(ceiling, ddG_foldx))
+          mutate(pos=as.numeric(str_sub(mut, 2, -2)))
+pred_ceil <- pred %>%
+          mutate(ddG_tetramer=mapply(ceiling_1, ddG_tetramer)) %>%
+          mutate(ddG_monomer=mapply(ceiling_2, ddG_monomer))
 plot_MSA_transformer(pred, 'graph/fit_vs_MSA_transformer.png')
-plot_ddG(pred, 'graph/fit_vs_foldX_ddG.png')
+plot_ddG(pred_ceil, 'graph/fit_vs_foldX_ddG_tetramer.png', 'tetramer')
+plot_ddG(pred_ceil, 'graph/fit_vs_foldX_ddG_monomer.png', 'monomer')
 print (paste('Pearson correlation between fitness and MSA: ',
        cor(pred$msa_transformer, pred$Fitness, method="pearson",use="complete.obs"), sep=''))
 print (paste('Spearman correlation between fitness and MSA: ',
        cor(pred$msa_transformer, pred$Fitness, method="spearman", use="complete.obs"), sep=''))
-print (paste('Pearson correlation between fitness and foldX_ddG: ',
-       cor(pred$ddG_foldx, pred$Fitness, method="pearson",use="complete.obs"), sep=''))
-print (paste('Spearman correlation between fitness and foldX_ddG: ',
-       cor(pred$ddG_foldx, pred$Fitness, method="spearman", use="complete.obs"), sep=''))
+print (paste('Pearson correlation between fitness and ddG_tetramer: ',
+       cor(pred$ddG_tetramer, pred$Fitness, method="pearson",use="complete.obs"), sep=''))
+print (paste('Spearman correlation between fitness and ddG_tetramer: ',
+       cor(pred$ddG_tetramer, pred$Fitness, method="spearman", use="complete.obs"), sep=''))
+print (paste('Pearson correlation between fitness and ddG_monomer: ',
+       cor(pred$ddG_monomer, pred$Fitness, method="pearson",use="complete.obs"), sep=''))
+print (paste('Spearman correlation between fitness and ddG_monomer: ',
+       cor(pred$ddG_monomer, pred$Fitness, method="spearman", use="complete.obs"), sep=''))
